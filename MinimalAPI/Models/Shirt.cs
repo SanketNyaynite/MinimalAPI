82540c4c07c40bece68a202551cc1dfda8ef2a65
SanketNyaynite <sanket.nyaynite@gmail.com>
2025-12-18 09:32:19 -0600

    Add Validation attribute to size and gender property

--- MinimalAPI/Models/Shirt.cs
+++ MinimalAPI/Models/Shirt.cs
@@ -1,18 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using MinimalAPI.Models.Validations;
+using System.ComponentModel.DataAnnotations;
 
 namespace MinimalAPI.Models
 {
     public class Shirt
     {
         public int ShirtId { get; set; }
-        [Required]                                  //Model validation using Data Annotations
+        [Required]                //Model validation using Data Annotations. Data Annotations are used on a single property of a model class
         public string? Brand { get; set; }
-        public int? Size { get; set; }                  //Question to say that size can be null
+
+        //Use Validation attribute like below when there are multiple properties to validate at the same time. In this case we are validating size and gender
+        [Shirt_EnsureCorrectSizing]
+        public int? Size { get; set; }                  //Question mark to say that size can be null
         [Required]
         public string? Color { get; set; }
         [Required]
         public string? Gender { get; set; }
         public double? Price { get; set; }
 
-    }
+    }                                             
 }
